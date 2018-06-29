61bfa413c197055a58d4ab55dcd55f6b2fa93227
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2018-06-29 19:43:18 +0530

    changed fetches url, in app.js, register.js and signin.js from localhost to the new heroku url

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
       .then(response => {
 
         if(response){
-          fetch('http://localhost:3000/image',{
+          fetch('https://hidden-plains-91756.herokuapp.com/image',{
                 method: 'put',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
